6e54ee902256aecfd48ee45b0c80df6caf007035
dylan horyza <horyza.dylan@gmail.com>
2024-01-21 16:11:58 -0700

    updated main file with page routes

---
+++ src/components/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react';
+// Bringing in the required component from 'react-router-dom' for linking between pages
+import { Link } from 'react-router-dom';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>home</h1>
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,47 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import HomePage from './components/pages/HomePage';
+import AboutPage from './components/pages/AboutPage';
+import ProjectPage from './components/pages/ProjectPage';
+import ContactPage from './components/pages/ContactPage';
+import ResumePage from './components/pages/ResumePage';
+import ErrorPage from './components/pages/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: '/about',
+        element: <AboutPage />,
+      },
+      {
+        path: '/projects',
+        element: <ProjectPage />,
+      },
+      {
+        path: 'contact',
+        element: <ContactPage />,
+      },
+      {
+        path: 'resume',
+        element: <ResumePage /> ,     
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router}/>
 )

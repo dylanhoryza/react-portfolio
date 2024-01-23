3e0148e84ee3e235e06025f5ae1f9f29777f9899
dylan horyza <horyza.dylan@gmail.com>
2024-01-23 12:41:35 -0700

    added global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import { Outlet } from 'react-router-dom';
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   
 
   return (
-    <>
+    <div className='app-container'>
       <Outlet />
-    </>
+
+    </div>
+      
+    
   )
 }
 

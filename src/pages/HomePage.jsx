7d04a6b6852714c81bfda712741f76eaae8bd777
dylan horyza <horyza.dylan@gmail.com>
2024-01-23 14:14:30 -0700

    created styles for homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,16 @@
 import { useState, useEffect } from 'react';
 // Bringing in the required component from 'react-router-dom' for linking between pages
 import { Link } from 'react-router-dom';
+import '../styles/homepage.css'
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>home</h1>
+    <div className='bg-image'>
+    <section className="short-bio text-center" id="home">
+    <h1 className="display-1">Dylan Horyza</h1>
+    <p className="display-4">Full Stack Developer</p>
+  </section>
     </div>
+
   )
 }

c794a750488b69ffcdaec4a5518b4ff0691a5d72
dylan horyza <horyza.dylan@gmail.com>
2024-01-24 14:26:46 -0700

    added footer and changed styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 import { useState } from 'react'
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header'
+import Footer from './components/Footer'
 import './reset.css'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  
-
   return (
-    <div className='app-container'>
-      <Header />
-      <Outlet />
-
+    <div className='app-wrapper'>
+      <div className='app-container'>
+        <Header />
+        <Outlet />
+      </div>
+      <Footer />
     </div>
-      
-    
-  )
+  );
 }
 
 export default App
